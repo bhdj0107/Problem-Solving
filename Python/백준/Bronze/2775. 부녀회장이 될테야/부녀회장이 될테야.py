ea1327f802d21c9c99d200326a75428c76f38c3e
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2024-03-05 17:36:20 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+    room = [[0 for _ in range(n)] for _ in range(k + 1)]
+    for i in range(k + 1):
+        room[i][0] = 1
+    for i in range(n):
+        room[0][i] = i + 1
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            room[i][j] = room[i-1][j] + room[i][j - 1]
+    print(room[k][n - 1])
