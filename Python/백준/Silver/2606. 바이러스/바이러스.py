6998d8318c9441104ab1cf7899d6c7ccdf502ae8
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2024-07-23 14:50:50 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 34036 KB -BaekjoonHub

--- Python/백준/Silver/2606. 바이러스/바이러스.py
+++ Python/백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,28 @@
-from collections import deque
 import sys
+from collections import deque
 
-def asd():
-    N = int(sys.stdin.readline())
-    chk = set()
-    chk.add(1)
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+connection = {i:[] for i in range(1, N + 1)}
 
-    pair = {i: set() for i in range(1, N + 1)}
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    connection[a].append(b)
+    connection[b].append(a)
 
-    M = int(sys.stdin.readline())
 
-    for i in range(M):
-        a, b = map(int, sys.stdin.readline().split())
-        pair[a].add(b)
-        pair[b].add(a)
+visited = [False for _ in range(N + 1)]
+stack = deque()
+stack.append(1)
 
-    chk.update(pair[1])
-    queue = deque(pair[1])
-    while queue:
-        temp = queue.popleft()
-        queue.extend(pair[temp] - chk)
-        chk.update(pair[temp])
-    print(len(chk) - 1)
+cnt = 0
+while stack:
+    now = stack.pop()
+    if (visited[now]): continue
+    else:
+        cnt += 1
+        visited[now] = True
+        for i in connection[now]:
+            stack.append(i)
 
-asd()
+print(cnt - 1)
