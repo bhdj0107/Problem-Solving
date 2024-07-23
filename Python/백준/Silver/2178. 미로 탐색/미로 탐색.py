800bc4d3748952c8140348f06e6e9b8e7e12aa6b
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2024-07-23 15:28:36 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 34052 KB -BaekjoonHub

--- Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,25 @@
+import sys
 from collections import deque
 
-N, M = map(int, input().split())
+N, M = map(int,sys.stdin.readline().split())
+field = [sys.stdin.readline().rstrip() for _ in range(N)]
 
 q = deque()
+q.append(((0, 0), (0, 0)))
+dist = [[0 for _ in range(M)] for _ in range(N)]
+
 dx = (1, 0, -1, 0)
-dy = (0 ,1, 0, -1)
-field = []
-check = []
-for i in range(N):
-	temp = input()
-	field.append([])
-	check.append([])
-	for j in range(M):
-		field[i].append(int(temp[j]))
-		check[i].append(0)
+dy = (0, 1, 0, -1)
 
-q.append([0,0])
-check[0][0] = 1
-
-while(1):
-	try:
-		temp = q.popleft()
-		for i in range(4):
-			nx = temp[0] + dx[i]
-			ny = temp[1] + dy[i]
-			if nx < 0 or ny < 0 or nx > M - 1 or ny > N - 1:
-				continue
-			if field[ny][nx] == 0 or check[ny][nx] != 0:
-				continue
-			q.append([nx, ny])
-			check[ny][nx] = check[temp[1]][temp[0]] + 1
-	except:
-		break
-
-print(check[N-1][M-1])
+while q:
+    prev, now = q.popleft()
+    if dist[now[0]][now[1]] != 0: continue
+    else:
+        dist[now[0]][now[1]] = dist[prev[0]][prev[1]] + 1
+        for k in range(4):
+            ny = now[0] + dy[k]
+            nx = now[1] + dx[k]
+            if ny >= 0 and ny < N and nx >= 0 and nx < M:
+                if field[ny][nx] == '1':
+                    q.append((now, (ny, nx)))
+print(dist[N - 1][M - 1])
