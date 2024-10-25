02f57bb8d6fc06d5a0682cc4b6999b5814264c13
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2024-10-25 19:57:18 +0900

    [Gold III] Title: 나무 재테크, Time: 800 ms, Memory: 184232 KB -BaekjoonHub

--- PyPy3/백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ PyPy3/백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,51 +1,49 @@
 import sys
-from collections import deque, defaultdict
-
-# f = open('./input.txt', 'r')
-# sys.stdin.readline = f.readline
-
+from collections import deque
 def a():
     N, M, K = map(int, sys.stdin.readline().split())
     adder = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
     trees = [tuple(map(int, sys.stdin.readline().split())) for _ in range(M)]
     tree_field = [[deque() for _ in range(N)] for _ in range(N)]
-
-    treecnt = 0
     for tree in trees:
         r, c, age = tree
         r, c = r - 1, c - 1
         tree_field[r][c].append(age)
-        treecnt += 1
 
     field = [[5 for _ in range(N)] for _ in range(N)]
-    grows = defaultdict(int)
+    deads = [[0 for _ in range(N)] for _ in range(N)]
+    grows = [[0 for _ in range(N)] for _ in range(N)]
     d = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
     for _ in range(K):
         # spring
         for i in range(N):
             for j in range(N):
-                dead = 0
                 for _ in range(len(tree_field[i][j])):
                     tree = tree_field[i][j].popleft()
                     if field[i][j] >= tree:
                         field[i][j] -= tree
                         tree_field[i][j].append(tree + 1)
-                        if (tree + 1) % 5 == 0: grows[(i, j)] += 1
-                    else: 
-                        dead += tree // 2
-                        treecnt -= 1
-                field[i][j] += dead + adder[i][j]
+                        if (tree + 1) % 5 == 0: grows[i][j] += 1
+                    else: deads[i][j] += tree // 2
                     
+        # summer, fall, winter
+        for i in range(N):
+            for j in range(N):
+                # summer
+                field[i][j] += deads[i][j] + adder[i][j]
+                deads[i][j] = 0
+                
+                # fall
+                if grows[i][j]:
+                    for k in range(8):
+                        ny, nx = i + d[k][0], j + d[k][1]
+                        if 0 <= ny < N and 0 <= nx < N:
+                            for _ in range(grows[i][j]): tree_field[ny][nx].appendleft(1)
+                    grows[i][j] = 0
 
-
-        # fall
-        for (i, j), grow in grows.items():
-            for k in range(8):
-                ny, nx = i + d[k][0], j + d[k][1]
-                if 0 <= ny < N and 0 <= nx < N:
-                    for _ in range(grow):
-                        tree_field[ny][nx].appendleft(1)
-                        treecnt += 1
-        grows.clear()
-    print(treecnt)
+    total = 0
+    for i in range(N):
+        for j in range(N):
+            total += len(tree_field[i][j])
+    print(total)
 a()
