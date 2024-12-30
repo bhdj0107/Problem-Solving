0e0338decd9e15de53c1654e8a4e597687418d53
bhdj0107 <43875772+bhdj0107@users.noreply.github.com>
2024-12-30 16:10:56 +0900

    [level 3] Title: 등굣길, Time: 2.32 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,13 @@
+def solution(m, n, puddles):
+    field = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    field[1][1] = 1
+    
+    puddles = set(map(tuple, puddles))
+    puddles.add((1, 1))
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if (j, i) not in puddles:
+                field[i][j] = (field[i - 1][j] + field[i][j - 1]) % 1000000007
+    
+    return field[n][m]
